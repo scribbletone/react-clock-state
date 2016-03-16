71784f598fac90476e02d36903c2803064f36068
Travis Kochel <travis@scribbletone.com>
2016-03-16 15:45:14 -0700

    time zones

--- src/ClockState.js
+++ src/ClockState.js
@@ -3,11 +3,17 @@ var React = require('react');
 var ClockState = {
   getInitialState: function() {
     return {
-      currentDate: new Date()
+      currentDate: new Date(),
+      timeZoneOffset: 0
     };
   },
   componentDidMount: function() {
     setInterval(this.setCurrentTime, 1000);
+  },
+  setTimeZoneOffset: function(hoursOffset){
+    this.setState({
+      timeZoneOffset: hoursOffset 
+    });
   },
   getCurrentHour: function(hourMode){
     hourMode = hourMode || 12;
@@ -24,9 +30,17 @@ var ClockState = {
   getCurrentSecond: function(){
     return this.state.currentDate.getSeconds();
   },
+  getUTCtime: function(){
+    var date = new Date();
+    var clientOffset = date.getTimezoneOffset();
+    return new Date(date.getTime() + (clientOffset * 60 * 1000));
+  },
   setCurrentTime: function(){
+    var dateUTC = this.getUTCtime();
+    var date = new Date(dateUTC.getTime() + (this.state.timeZoneOffset * 60 * 60 * 1000));
+
     this.setState({
-      currentDate: new Date()
+      currentDate: date
     });
   },
   formattedTime: function(opts){
